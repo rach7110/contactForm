553b38b985b9714d0e59d35cfb896086b73843b9
Rachel <rachel.loziuk@gmail.com>
2017-11-16 17:51:44 -0600

    create a view for email message

---
+++ resources/views/emails/contact_form.blade.php
@@ -0,0 +1,8 @@
+<p>You have a new message!</p>
+<p>From: {{$contactMessage->full_name}}</p>
+<p>Email: {{$contactMessage->email}}</p>
+<p>Telephone: {{$contactMessage->telephone}}</p>
+<p>Message:</p>
+<div>
+    {{$contactMessage->telephone}}
+</div>
