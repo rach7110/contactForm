749a16e7d064d77179fc4718946205cb47e33ee7
Rachel <rachel.loziuk@gmail.com>
2017-11-17 15:19:05 -0600

    testing: check message has properties

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -9,15 +9,33 @@ use App\Message;
 
 class MessageTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testMessageHasFullNameAttribte()
-    {
-        $message = new Message;
+   public function setup()
+   {
+        parent::setUp();
+        $this->message = new Message;
+   }
 
-        $this->assertTrue(true);
+    public function testMessageHasFullname()
+    {
+        $this->message->full_name = 'Sam Smith';
+        $this->assertEquals('Sam Smith', $this->message->full_name);
+    }
+
+    public function testMessageHasEmail()
+    {
+        $this->message->email = 'sam@smith.com';
+        $this->assertEquals('sam@smith.com', $this->message->email);
+    }
+
+    public function testMessageHasTelephone()
+    {
+        $this->message->telephone = '322-444-6555';
+        $this->assertEquals('322-444-6555', $this->message->telephone);
+    }
+
+    public function testMessageHasDescription()
+    {
+        $this->message->description = 'The grey fox jumps over the fence';
+        $this->assertEquals('The grey fox jumps over the fence', $this->message->description);
     }
 }
